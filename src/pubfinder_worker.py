57259a38de3aac62a496eb8cc1b46158afd65c36
Lukas Jesche <ljaschka@gmail.com>
2021-07-20 12:21:03 +0200

    - working on config

--- src/pubfinder_worker.py
+++ src/pubfinder_worker.py
@@ -1,8 +1,9 @@
 import logging
 import time
+import uuid
 
 import requests
-from multiprocessing.context import Process
+import pymongo
 
 from event_stream.event_stream_consumer import EventStreamConsumer
 from event_stream.event_stream_producer import EventStreamProducer
@@ -16,6 +17,24 @@ class PubFinderWorker(EventStreamConsumer, EventStreamProducer):
     log = "PubFinderWorker "
     group_id = "pub-finder-worker"
 
+    mongoClient = False
+
+    config = {
+        'mongo_url': "mongodb://mongo_db:27017/",
+        'mongo_client': "events",
+        'mongo_collection': "publication",
+    }
+
+    def __init__(self, id):
+        super().__init__(id)
+        # todo client for each thread
+        self.mongoClient = pymongo.MongoClient(host=self.config['mongo_url'],
+                                               serverSelectionTimeoutMS=3000,  # 3 second timeout
+                                               username="root",
+                                               password="example"
+                                               )
+        self.db = self.mongoClient[self.config['mongo_client']]
+        self.collection = self.db[self.config['mongo_collection']]
 
     def on_message(self, json_msg):
         logging.warning(self.log + "on message pubfinder worker")
@@ -23,15 +42,26 @@ class PubFinderWorker(EventStreamConsumer, EventStreamProducer):
         e = Event()
         e.from_json(json_msg)
 
+        # todo set id as uuid int
         publication = self.get_data_from_crossref(e.data['obj']['data']['doi'])
         if publication:
             logging.warning(self.log + "linked publication crossref")
+
+            cp = publication
+            try:
+                # save publication to db
+                cp['_id'] = uuid.uuid4().hex
+                self.collection.insert_one(cp)
+            except pymongo.errors.DuplicateKeyError:
+                logging.warning("MongoDB collection/state%s, Duplicate found, continue" % json_msg['state'])
+
+            # set event to linked
             e.data['obj']['data'] = publication
+            e.data['obj']['data']['doi'] = e.data['obj']['data']['DOI']  # todo
             # event.data['obj']['pid'] = publication['id']
             # event.set('obj_id', )
             e.set('state', 'linked')
             self.publish(e)
-
 
     def get_data_from_crossref(self, doi):
         base_url = "https://api.crossref.org/works/"
@@ -49,7 +79,8 @@ class PubFinderWorker(EventStreamConsumer, EventStreamProducer):
 
 if __name__ == '__main__':
     logging.warning("start Mongo DB connector")
-    time.sleep(15)
-
+    time.sleep(10)
     e = PubFinderWorker(1)
+    time.sleep(5)
+
     e.consume()
