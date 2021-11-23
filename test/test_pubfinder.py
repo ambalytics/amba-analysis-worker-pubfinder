abd1af3e61c026eb82dcd06b99858848cb8cb5e9
Lukas Jesche <ljaschka@gmail.com>
2021-11-23 10:35:18 +0100

    fix: daemon timer threads

--- test/test_pubfinder.py
+++ test/test_pubfinder.py
@@ -25,10 +25,24 @@ class TestPubfinder(unittest.TestCase):
                 logging.warning('sleep')
                 pass
             else:
-                logging.warning(item['item'])
-                logging.warning(item['tag'])
-                logging.warning(item['item'].data['obj'])
-                self.assertEqual(item.item.data['obj']['data'], '10.1016/j.yjmcc.2021.05.007')
+                self.assertEqual(item['tag'], 'openaire')
+                self.assertEqual(item.item.data['obj']['data']['title'],
+                                 'The origin of extracellular fields and currents — EEG, ECoG, LFP and spikes')
+                self.assertEqual(item.item.data['obj']['data']['normalized_title'],
+                                 'the origin of extracellular fields and currents  eeg ecog lfp and spikes')
+                self.assertEqual(item.item.data['obj']['data']['publisher'], 'Nature Publishing Group')
+                self.assertEqual(item.item.data['obj']['data']['abstract'],
+                                 'Neuronal activity in the brain gives rise to transmembrane currents that can be '
+                                 'measured in the extracellular medium. Although the major contributor of the '
+                                 'extracellular signal is the synaptic transmembrane current, other sources — '
+                                 'including Na+ and Ca2+ spikes, ionic fluxes through voltage- and ligand-gated '
+                                 'channels, and intrinsic membrane oscillations — can substantially shape the '
+                                 'extracellular field. High-density recordings of field activity in animals and '
+                                 'subdural grid recordings in humans, combined with recently developed data '
+                                 'processing tools and computational modelling, can provide insight into the '
+                                 'cooperative behaviour of neurons, their average synaptic input and their spiking '
+                                 'output, and can increase our understanding of how these processes contribute to the '
+                                 'extracellular signal.')
                 os.running = False
                 break
 
