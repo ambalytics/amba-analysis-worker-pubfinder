0bd3f0f6305a52f95a448810325716175e652890
Lukas Jesche <ljaschka@gmail.com>
2021-11-23 19:27:46 +0100

    fix: imports

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@
 ./scripts/wait-for-it.sh "$KAFKA_BOOTRSTRAP_SERVER" -t 5 -- echo "Kafka started"
 ./scripts/wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" -t 5 -- echo "Postgres started"
 
-python -m src.pubfinder_worker.py
+python -m src.pubfinder_worker
