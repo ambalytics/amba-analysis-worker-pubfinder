ec7ee76f58def8d5a86d14ccab7949f6cc22aa82
Lukas Jesche <ljaschka@gmail.com>
2021-11-23 10:42:40 +0100

    feat: semanticscholar test

--- test/test_pubfinder.py
+++ test/test_pubfinder.py
@@ -5,12 +5,13 @@ from collections import deque
 
 from event_stream.event import Event
 from src import openaire_source
+from src import semanticscholar_source
 
 
 class TestPubfinder(unittest.TestCase):
 
     def test_open_aire_source(self):
-        logging.warning('start testing')
+        logging.warning('start testing open aire')
         result_queue = deque()
         os = openaire_source.OpenAireSource(result_queue)
         e = Event()
@@ -31,6 +32,44 @@ class TestPubfinder(unittest.TestCase):
                 self.assertEqual(item['item'].data['obj']['data']['normalized_title'],
                                  'the origin of extracellular fields and currents  eeg ecog lfp and spikes')
                 self.assertEqual(item['item'].data['obj']['data']['publisher'], 'Nature Publishing Group')
+                self.assertEqual(item['item'].data['obj']['data']['abstract'],
+                                 'Neuronal activity in the brain gives rise to transmembrane currents that can be '
+                                 'measured in the extracellular medium. Although the major contributor of the '
+                                 'extracellular signal is the synaptic transmembrane current, other sources — '
+                                 'including Na+ and Ca2+ spikes, ionic fluxes through voltage- and ligand-gated '
+                                 'channels, and intrinsic membrane oscillations — can substantially shape the '
+                                 'extracellular field. High-density recordings of field activity in animals and '
+                                 'subdural grid recordings in humans, combined with recently developed data '
+                                 'processing tools and computational modelling, can provide insight into the '
+                                 'cooperative behaviour of neurons, their average synaptic input and their spiking '
+                                 'output, and can increase our understanding of how these processes contribute to the '
+                                 'extracellular signal.')
+                os.running = False
+                break
+
+    def test_semanticscholar_source(self):
+        logging.warning('start testing semantic scholar')
+        result_queue = deque()
+        os = semanticscholar_source.SemanticScholarSource(result_queue)
+        e = Event()
+        e.data['obj']['data'] = {'doi': '10.1038/nrn3241', 'source_id': [{'title': 'test'}]}
+        os.work_queue.append(e)
+        start = time.time()
+        while time.time() - start < 10:
+            try:
+                item = result_queue.pop()
+            except IndexError:
+                time.sleep(1)
+                logging.warning('sleep')
+                pass
+            else:
+                logging.warning(item['item'].data['obj']['data'])
+                self.assertEqual(item['tag'], 'semanticscholar')
+                self.assertEqual(item['item'].data['obj']['data']['title'],
+                                 'The origin of extracellular fields and currents — EEG, ECoG, LFP and spikes')
+                self.assertEqual(item['item'].data['obj']['data']['normalized_title'],
+                                 'the origin of extracellular fields and currents  eeg ecog lfp and spikes')
+                self.assertEqual(item['item'].data['obj']['data']['publisher'], 'Nature Reviews Neuroscience')
                 self.assertEqual(item['item'].data['obj']['data']['abstract'],
                                  'Neuronal activity in the brain gives rise to transmembrane currents that can be '
                                  'measured in the extracellular medium. Although the major contributor of the '
